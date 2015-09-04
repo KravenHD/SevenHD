afc5e058049b26749ee92ca1000bb0b73a5b71d2
KravenHD <kravenhd@web.de>
2015-09-04 13:13:49 +0200

    optimize SevenHDUpdate

--- usr/lib/enigma2/python/Components/Converter/SevenHDUpdate.py
+++ usr/lib/enigma2/python/Components/Converter/SevenHDUpdate.py
@@ -1,111 +1,81 @@
 # -*- coding: utf-8 -*-
-from Components.config import config
-from Components.Converter.Converter import Converter
-from Components.Element import cached
-from urllib2 import urlopen
+import os
 from enigma import eTimer
+from Components.config import config
+from Components.Element import cached
+from Tools.Directories import fileExists
+from twisted.web.client import downloadPage
+from Components.Converter.Converter import Converter
 
-url = "http://www.gigablue-support.org/skins/SevenHD/update/version.txt"
+TMP_FILE = '/tmp/SevenHDversion.txt'
+URL = 'http://www.gigablue-support.org/skins/SevenHD/update/version.txt'
 
 class SevenHDUpdate(Converter, object):
-
 	def __init__(self, type):
 		Converter.__init__(self, type)
-		
+                
                 if type == 'Update':
                    self.type = 1
-                else:
-                   self.type = -1
-		
+
                 self.check_timer = eTimer()
-                self.check_timer.callback.append(self.getText)
-                self.check_timer.start(60000)
-					
+                self.check_timer.callback.append(self.get)
+                self.check_timer.start(30000)
+	
 	@cached
 	def getText(self):
-	    version = config.plugins.SevenHD.version.value
-            box_version = version
-            if self.type == 1:
-               try:
-                   new_version = urlopen(url).read()
-                   online_version = new_version
-               except:
-                   new_version = '0.0.0'
-                   online_version = 'N/A'
-               
-               version = ''.join(version.replace('.',''))
-               new_version = ''.join(new_version.replace('.',''))
-                 
-               if str(len(version)) <= str('3'): 
-                   version = version + str('0')
-                   if str(len(version)) < str('4'): 
-                      version = version + str('0')
-               if str(len(new_version)) <= str('3'): 
-                   new_version = new_version + str('0')
-                   if str(len(new_version)) < str('4'): 
-                      new_version = new_version + str('0')
-                      
-               if int(version) < int(new_version):
-                   self.update_available = 'Last Version on Server ' + str(online_version)
-               else:
-                   self.update_available = 'Last Version on Server ' + str(box_version)
-                   
-               return self.update_available
-            else:
-               return version
+            self.info = self.look('1')
+            return str(self.info)
+
 	text = property(getText)
-################################################################################
-        @cached
-	def getValue(self):
-	    version = config.plugins.SevenHD.version.value
-            if self.type == 1:
-               try:
-                   new_version = urlopen(url).read()
-               except:
-                   new_version = '0.0.0'
-                
-               version = ''.join(version.replace('.',''))
-               new_version = ''.join(new_version.replace('.',''))
-                 
-               if str(len(version)) <= str('3'): 
-                   version = version + str('0')
-                   if str(len(version)) < str('4'): 
-                      version = version + str('0')
-               if str(len(new_version)) <= str('3'): 
-                   new_version = new_version + str('0')
-                   if str(len(new_version)) < str('4'): 
-                      new_version = new_version + str('0')
-               
-               if int(version) < int(new_version):
-                   self.update_available = 1
-               else:
-                   self.update_available = -1
-	
-               return self.update_available
-            else:
-               return -1
-	value = property(getValue)
-################################################################################
+
         @cached
 	def getBoolean(self):
-	    version = config.plugins.SevenHD.version.value
-            if self.type == 1:
-               try:
-                   new_version = urlopen(url).read()
-               except:
-                   new_version = '0.0.0'
-                
-               version = ''.join(version.replace('.',''))
-               new_version = ''.join(new_version.replace('.',''))
-               
-               if int(version) < int(new_version):
-                   return True
-               else:
-                   return False
-	
-            else:
-               return False
+	    self.info = self.look('2')
+            return self.info
+
 	boolean = property(getBoolean)
-################################################################################
-	def changed(self, what):
+
+        def get(self):        
+            downloadPage(URL, TMP_FILE).addErrback(self.error)
+
+        def look(self, what):
+            box_version = config.plugins.SevenHD.version.value
+
+            if not fileExists(TMP_FILE):
+               self.get() 
+            content = os.popen("cat %s" % TMP_FILE).read()
+
+            version = box_version
+            new_version = content
+
+            version = version.replace('.','')
+            online_version = content.replace('.','')
+
+	    if str(len(version)) <= str('3'): 
+               version = version + str('0')
+               if str(len(version)) < str('4'): 
+                  version = version + str('0')
+            if str(len(online_version)) <= str('3'): 
+               online_version = online_version + str('0')
+               if str(len(online_version)) < str('4'): 
+                  online_version = online_version + str('0')
+
+            if int(version) < int(online_version):
+                if str(what) == str('1'):   
+                   self.update_available = 'Last Version on Server ' + str(new_version)
+                else:
+                   self.update_available = True
+            else:
+                if str(what) == str('1'):   
+                   self.update_available = 'Last Version on Server ' + str(box_version)
+                else:
+                   self.update_available = False
+
+            return self.update_available
+
+        def error(self, error):
+            box_version = config.plugins.SevenHD.version.value
+            os.system('echo "' + box_version + '" > ' + TMP_FILE)
+
+        def changed(self, what):
 	    Converter.changed(self, (self.CHANGED_POLL,))
